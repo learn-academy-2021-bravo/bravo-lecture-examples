5daa1d572cc35bebe3b18f21aac2ef56c8371e38
Mina Chuong <mina.chuong@gmail.com>
2021-05-04 14:16:58 -0700

    Updated javascript-intro

--- javascript-intro.js
+++ javascript-intro.js
@@ -21,12 +21,61 @@
 
   // console.log(5 ** 4)
   // modulo - return the remainder of a division problem
-  console.log(5 % 3)
+  // console.log(5 % 3)
+  // console.log(100 % 10)
+  // console.log(2938739857349875348975)
   // atom - log + tab
   // vs code - cl + tab
 
 
-// String
+// String - a collection of characters wrapped with double or single quotations
+// console.log("Hello, Bravo")
+// console.log('Hello, Bravo')
+// console.log("42")
+// console.log(42)
+
+// String concatenation - joining multiple strings
+// console.log("HELLO" + "WORLD")
+// console.log("HELLO" + " WORLD")
+// console.log("DIE''GO")
+// console.log('DIE "GO"')
+
+// .length - get the length of a string
+// console.log("BRAVO".length)
+// console.log("BRAVO COHORT".length)
+// console.log("HELLO WORLD KSJLKSDJFLKSDJFLKSDJF".length)
+
+// Index - used to access a character within a string; location of a character; square brackets;
+// Javascript is zero-based, meaning the order/location begins at zero 
+
+// console.log("BRAVO"[0])
+// outputs: "B"
+// console.log("BRAVO"[1])
+// outputs: "R"
+
+// Built-in methods: pre-written functionality within the language
+
+// .indexOf() - returns the location of the provided character within a string
+// console.log("BRAVO".indexOf("B"))
+
+// .toUpperCase() - returns a string of upper-cased letter(s)
+console.log("these are all lowercased".toUpperCase())
+// outputs: "THESE ARE ALL LOWERCASED"
+
+// .charAt() - returns a character at the specific index
+console.log("BRAVO".charAt(0))
+
+// Type coercion - feature of Javascript where operations between different data types are still evaluated
+
+// console.log("hello" + 1)
+// outputs: "hello1"
+
+// console.log(100 + "hello world")
+// console.log(100 + "2")
+// console.log("23342" + 1)
+
+
+
 // Boolean
 // Null
 // Undefined
