4c42464479b82a276a9c7c59c539375b6d78f021
sjproctor <sjproctor950@gmail.com>
2021-05-04 12:06:14 -0700

    starting javascript lecture notes

---
+++ javascript-intro.js
@@ -0,0 +1,33 @@
+// JavaScript
+
+// Dynamic, interpreted, scripting language
+// JavaScript file should be lowercase letters, no spaces, .js extension
+// run the file $ node <filename>
+// comments are ignored by the processor - // or command + ?/
+
+// Data Types
+// 6 primitive data types
+
+// Number
+  // Integers
+  42
+  // Floats
+  3.14
+  // Math
+  5 + 3
+  5 - 2
+  5 * 4
+  15 / 3
+
+  // console.log(5 ** 4)
+  // modulo - return the remainder of a division problem
+  console.log(5 % 3)
+  // atom - log + tab
+  // vs code - cl + tab
+
+
+// String
+// Boolean
+// Null
+// Undefined
+// Symbol
